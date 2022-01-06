ecd88d4afab8a22529cceb2ecaad4f4d683de5b9
naraja03 <santhanraja3@gmail.com>
2022-01-06 20:46:39 +0530

    commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="nav">
+    <ul class="navigation">
+        <li class="logo nav_item"><img src="../assets/logo.jpg" alt="Naraja"></li>
+        <li class="home nav_item">HOME</li>
+        <li class="about nav_item">ABOUT</li>
+        <li class="projects nav_item">PROJECTS</li>
+        <li class="skills nav_item">SKILLS</li>
+        <li class="contactUs nav_item">CONTACT US</li>
+        <li class="socialMedia">
+            <div class="social_link">twitter</div>
+            <div class="social_link">github</div>
+            <div class="social_link">insta</div>
+        </li>
+    </ul>
+</div>
+</template>
