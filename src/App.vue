341dd298da9ab041d2b21f83230401f3d808d4b7
naraja03 <santhanraja3@gmail.com>
2022-01-07 19:00:38 +0530

    3

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,5 @@ export default {
   components:{
     NavBar,
   },
-  setup() {
-  },
 }
 </script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="projetPage">
-        <h1 class="project_heading">My Projects</h1>
-    </div>
+  <div class="projectPage">
+    <h1 class="project_heading">My Projects</h1>
+  </div>
 </template>
